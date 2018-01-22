4a3bae423997827b22e046470091e5e533cea695
ailiefraser <cafraser@cs.ucsd.edu>
2018-01-22 15:33:28 -0800

    debugging php

--- save.php
+++ save.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if (isset($_POST['event'])) {
+if (isset($_GET['event'])) {
 
 	$event_headers = array("user ID", "time", "video", "event", "video time", "new video time", "playback speed", "screen mode");
 
@@ -12,7 +12,7 @@ if (isset($_POST['event'])) {
 		$event_data = array($event_headers);
 	}
 
-	array_push($event_data, array($_SESSION['id'], date(), $_SESSION['current_video'], $_POST['event'], "", "", "", ""));
+	array_push($event_data, array($_SESSION['id'], date(), $_SESSION['current_video'], $_GET['event'], "", "", "", ""));
 
 	$events_file = fopen("data/video_events.csv", "a");
 

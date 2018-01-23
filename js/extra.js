b7fd1097d3fa8a7b972a0c7dd1d12e2c417a77d3
ailiefraser <cafraser@cs.ucsd.edu>
2018-01-22 19:08:38 -0800

    Updating php saving

--- js/extra.js
+++ js/extra.js
@@ -129,7 +129,7 @@ function updateSizes() {
 
 	$("#player_mask").width($("#player").width());
 	$("#player_mask").height($("#player").height());
-	$("#player_mask").offset($("#player").offset());
+	$("#player_mask").position($("#player").position());
 }
 
 function saveEvent(event, video_time) {

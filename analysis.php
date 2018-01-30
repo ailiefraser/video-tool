cb39910a3621abceae1a40780f34bed9af28e6c3
ailiefraser <cafraser@cs.ucsd.edu>
2018-01-29 16:31:48 -0800

    working on analysis

--- analysis.php
+++ analysis.php
@@ -2,68 +2,94 @@
 
 session_start();
 
-if (file_exists("data/events.csv")) {
-	$events_file = fopen("data/events.csv", "r");
-	$events = array();
-	$events_header = null;
-	while ($row = fgetcsv($events_file)) {
-		if ($events_header === null) {
-	        $events_header = $row;
-	        continue;
-	    }
-	    $events[] = array_combine($events_header, $row);
-	}
-	fclose($events_file);
+if (isset($_POST['video'])) {
 
-	$videos = array();
-	$users = array();
-	
-	foreach ($events as $event) {
-		if (!in_array($event["video"], $videos)) {
-			array_push($videos, $event["video"]);
+	$cur_video = $_POST['video'];
+
+	if (file_exists("data/events.csv")) {
+		$events_file = fopen("data/events.csv", "r");
+		$events = array();
+		$events_header = null;
+		while ($row = fgetcsv($events_file)) {
+			if ($events_header === null) {
+		        $events_header = $row;
+		        continue;
+		    }
+		    $events[] = array_combine($events_header, $row);
 		}
-		if (!in_array($event["user ID"], $users)) {
-			array_push($users, $event["user ID"]);
-		}
-	}
+		fclose($events_file);
 
-	$user_data = array();
-
-	foreach ($users as $user) {
-		$user_data[$user] = array();
-		$user_playing = false;
-		$start_time = 0;
+		$users = array();
+		
+		// get all events from this video, organized by user
 		foreach ($events as $event) {
-			if (strcmp($event["user ID"], $user) == 0) {
-				// it's this user
-				if (!$user_playing && in_array($event["event"], array("play video", "restart video"))) {
-					// if user was not playing and this is a play event
-					if (strcmp($event["event"], "play video") == 0) {
-						$start_time = $event["video time"];
-					} else {
-						$start_time = $event["new video time"];
-					}
-					$cur_video = $event["video"];
-					$user_playing = true;
-				} else if ($user_playing && 
-					in_array($event["event"], array("load video", "pause video", "video ended", "restart video", "seek video"))) {
-					// if user was playing and this event stopped it
-					array_push($user_data[$user], 
-						array("start_time" => $start_time, "end_time" => $event["video time"], "video" => $cur_video));
-					if (in_array($event["event"], array("restart video", "seek video"))) {
-						// user is still playing but started from a new spot
-						$start_time = $event["new video time"];
-					} else {
-						// user stopped playing
-						$user_playing = false;
+			if (strcmp($event["video"], $cur_video) == 0 && 
+					!in_array($event["user ID"], $users)) {
+
+				array_push($users, $event["user ID"]);
+			}
+		}
+
+		$user_data = array();
+
+		foreach ($users as $user) {
+			$user_data[$user] = array();
+			$user_playing = false;
+			$start_time = 0;
+			foreach ($events as $event) {
+				if (strcmp($event["user ID"], $user) == 0) {
+					// it's this user
+					if (!$user_playing && in_array($event["event"], array("play video", "restart video"))) {
+						// if user was not playing and this is a play event
+						if (strcmp($event["event"], "play video") == 0) {
+							$start_time = $event["video time"];
+						} else {
+							$start_time = $event["new video time"];
+						}
+						$cur_video = $event["video"];
+						$user_playing = true;
+					} else if ($user_playing && 
+						in_array($event["event"], array("load video", "pause video", "video ended", "restart video", "seek video"))) {
+						// if user was playing and this event stopped it
+						array_push($user_data[$user], 
+							array("start_time" => $start_time, "end_time" => $event["video time"], "video" => $cur_video));
+						if (in_array($event["event"], array("restart video", "seek video"))) {
+							// user is still playing but started from a new spot
+							$start_time = $event["new video time"];
+						} else {
+							// user stopped playing
+							$user_playing = false;
+						}
 					}
 				}
 			}
 		}
+		var_dump($user_data);
 	}
-
 } else {
-	$videos = array();
+	if (file_exists("data/events.csv")) {
+		$events_file = fopen("data/events.csv", "r");
+		$events = array();
+		$events_header = null;
+		while ($row = fgetcsv($events_file)) {
+			if ($events_header === null) {
+		        $events_header = $row;
+		        continue;
+		    }
+		    $events[] = array_combine($events_header, $row);
+		}
+		fclose($events_file);
+
+		$videos = array();
+		
+		foreach ($events as $event) {
+			if (!in_array($event["video"], $videos)) {
+				array_push($videos, $event["video"]);
+			}
+		}
+	} else {
+		$videos = array();
+	}
 }
 // "user ID",time,video,event,"video time","new video time","playback speed","screen mode"
 ?>

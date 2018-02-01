cb86290821384a76756704dcdde2b8c48e3ed91d
ailiefraser <cafraser@cs.ucsd.edu>
2018-02-01 12:31:22 -0800

    working on analysis

--- js/analysis.js
+++ js/analysis.js
@@ -191,9 +191,9 @@ function getGradientValue(value) {
 	var idx2  = idx1+1;                        // ... and before this index (inclusive).
 	var fractBetween = value - Math.float(idx1);    // Distance between the two indexes (0-1).
 
-	var red   = (color[idx2][0] - color[idx1][0])*fractBetween + color[idx1][0];
-	var green = (color[idx2][1] - color[idx1][1])*fractBetween + color[idx1][1];
-	var blue  = (color[idx2][2] - color[idx1][2])*fractBetween + color[idx1][2];
+	var red   = Math.round((color[idx2][0] - color[idx1][0])*fractBetween + color[idx1][0]);
+	var green = Math.round((color[idx2][1] - color[idx1][1])*fractBetween + color[idx1][1]);
+	var blue  = Math.round((color[idx2][2] - color[idx1][2])*fractBetween + color[idx1][2]);
 
 	return `rgba(${red}, ${green}, ${blue}, 1)`;
 }

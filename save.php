cc71f066fa9a1ae610e148ecc4f36417459997fe
ailiefraser <cafraser@cs.ucsd.edu>
2018-02-12 14:26:36 -0800

    trying captions

--- save.php
+++ save.php
@@ -12,8 +12,14 @@ if (isset($_POST['event'])) {
 		$event_data = array($event_headers);
 	}
 
+	if (isset($_POST["playback_speed"])) {
+		$playback_speed = $_POST["playback_speed"];
+	} else {
+		$playback_speed = 1;
+	}
+
 	array_push($event_data, array($_SESSION['id'], date('c'), $_POST['current_video'], $_POST['event'], 
-		$_POST['video_time'], $_POST["new_video_time"], $_POST["playback_speed"], $_POST["screen_mode"]));
+		$_POST['video_time'], $_POST["new_video_time"], $playback_speed, $_POST["screen_mode"]));
 
 	//var_dump($event_data);
 

70694e78ca904275d5d89232dadc794307f58a1f
ailiefraser <cafraser@cs.ucsd.edu>
2018-02-12 14:34:51 -0800

    trying captions

--- analysis.php
+++ analysis.php
@@ -73,8 +73,7 @@ if (isset($_POST['video'])) {
 		}
 		// get captions
 		$captions_file = file_get_contents(
-			'https://www.googleapis.com/youtube/v3/captions?id='.$cur_video.
-			'&key='.$api_key);
+			'https://www.googleapis.com/youtube/v3/captions?id='.$cur_video);
 		var_dump($captions_file);
 	}
 } else {
